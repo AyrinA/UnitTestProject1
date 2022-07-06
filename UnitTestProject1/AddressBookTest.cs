1a6f20b41313696c7e2f663b763acafe266ddcdd
AyrinA <ayrin.sb8@gmail.com>
2022-07-06 10:13:53 +0300

    ContactsTests

--- UnitTestProject1/AddressBookTest.cs
+++ UnitTestProject1/AddressBookTest.cs
@@ -10,7 +10,7 @@ using OpenQA.Selenium.Support.UI;
 namespace addressbook_web_tests
 {
     [TestFixture]
-    public class UntitledTestCase
+    public class AddressBookTest
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -40,38 +40,66 @@ namespace addressbook_web_tests
         }
 
         [Test]
-        public void TheUntitledTestCaseTest()
+        public void AddressBookTests()
         {
-            driver.Navigate().GoToUrl(baseURL);
-            driver.FindElement(By.Name("user")).Click();
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Click();
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
+            OpenHomePage();
+            Login("admin","secret");
+            GoToGroupsPage();
+            InitGroupCreation();
+            FillGroupForm("word", "word2", "word3");
+            SubmitGroupCreation();
+            ReturnToGroupPage();
+        }
+
+        private void ReturnToGroupPage()
+        {
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+
+        private void SubmitGroupCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm(string name, string header, string footer)
+        {
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("group2");
+            driver.FindElement(By.Name("group_name")).SendKeys(name);
             driver.FindElement(By.Name("group_header")).Click();
             driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("logo2");
+            driver.FindElement(By.Name("group_header")).SendKeys(header);
             driver.FindElement(By.Name("group_footer")).Click();
             driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("comment2");
-            driver.FindElement(By.Name("submit")).Click();
-            driver.FindElement(By.LinkText("group page")).Click();
-            driver.FindElement(By.LinkText("Logout")).Click();
+            driver.FindElement(By.Name("group_footer")).SendKeys(footer);
+        }
+
+        private void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void Login(string username, string password)
+        {
             driver.FindElement(By.Name("user")).Click();
             driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
+            driver.FindElement(By.Name("user")).SendKeys("username");
             driver.FindElement(By.Name("pass")).Click();
             driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.Id("LoginForm")).Submit();
+            driver.FindElement(By.Name("pass")).SendKeys("password");
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
         }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
         private bool IsElementPresent(By by)
         {
             try
---
+++ UnitTestProject1/ContactsData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class ContactsData
+    {
+        private string firstname;
+        private string lastname;
+        private string company = "";
+        private string mobile = "";
+        private string email = "";
+        private string address2 = "";
+
+        public ContactsData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public ContactsData(string firstname, string lastname, string company, string mobile, string email, string address2)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.company = company;
+            this.mobile = mobile;
+            this.email = email;
+            this.address2 = address2;
+        }
+
+        public string Firstname
+        { get { return firstname; } set { firstname = value; } }
+
+        public string Lastname
+        { get { return lastname; } set { lastname = value; } }
+
+        public string Company
+        { get { return company; } set { company = value; } }
+
+        public string Mobile
+        { get { return mobile; } set { mobile = value; } }
+
+        public string Email
+        { get { return email; } set { email = value; } }
+
+        public string Address2
+        { get { return address2; } set { address2 = value; } }
+    }
+}
